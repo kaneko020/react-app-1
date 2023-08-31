4a2c96c8b2b74cac803b8ab35a205763bf070dce
kaneko_n <kaneko_n@thinkone.jp>
2023-08-31 12:55:36 +0900

    header レイアウト

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -13,22 +13,22 @@ const Header = () => {
         <h1 className="pc:flex hidden text-[35px] font-semibold">Introduction</h1>
       </a>
 
-      <div className="flex items-center mx-2 pc:gap-7 gap-5 text-[18px] font-medium">
-        <a href="/" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+      <div className="flex items-center mx-2 pc:gap-7 gap-6 pc:text-[18px] text-[14px] font-medium">
+        <a href="/" className="flex pc:flex-row flex-col items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
           <PiHouseDuotone className="text-[30px] pc:mr-2" />
-          <p className="pc:flex hidden">Home</p>
+          <p className="pc:m-0 -mt-1">Home</p>
         </a>
         <span className="pc:flex hidden">|</span>
 
-        <a href="/comics" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+        <a href="/comics" className="flex pc:flex-row flex-col items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
           <PiBooksDuotone className="text-[30px] pc:mr-2" />
-          <p className="pc:flex hidden">Comics</p>
+          <p className="pc:m-0 -mt-1">Comics</p>
         </a>
         <span className="pc:flex hidden">|</span>
 
-        <a href="/movies" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+        <a href="/movies" className="flex pc:flex-row flex-col items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
           <PiVideoDuotone className="text-[30px] pc:mr-2" />
-          <p className="pc:flex hidden">Movies</p>
+          <p className="pc:m-0 -mt-1">Movies</p>
         </a>
       </div>
     </div>
