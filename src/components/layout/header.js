16493d0f362c22da4f60e1cf47117eac6e310d7c
kaneko_n <kaneko_n@thinkone.jp>
2023-08-31 11:00:08 +0900

    header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,4 +1,8 @@
-import { FaBook } from 'react-icons/fa';
+import { PiHouseDuotone } from 'react-icons/pi';
+import { PiBooksDuotone } from 'react-icons/pi';
+import { PiVideoDuotone } from 'react-icons/pi';
+import { PiPlugsDuotone } from 'react-icons/pi';
+import { PiPlugsConnectedDuotone } from 'react-icons/pi';
 
 const Header = () => {
   return (
@@ -6,13 +10,25 @@ const Header = () => {
       className="flex sticky top-0 left-0 w-full h-[100px] justify-between items-center text-[#333] bg-gray-200 bg-opacity-60 z-10"
     >
       <a href="/" className="flex items-center mx-6 z-20 hover:text-[#555]">
-        <FaBook className="text-[50px] mr-2" />
         <h1 className="text-[35px] font-semibold">Introduction</h1>
       </a>
-      <div className="flex items-center mx-8 gap-6 text-[18px] font-medium">
-        <a href="/">Home</a>
-        <a href="/comics">Comics</a>
-        <a href="/movies">Movies</a>
+      <div className="flex items-center mx-8 gap-7 text-[18px] font-medium">
+        <a href="/" className="flex items-center">
+          <PiHouseDuotone className="text-[25px] mr-2" />
+          Home
+        </a>
+        <span>|</span>
+
+        <a href="/comics" className="flex items-center">
+          <PiBooksDuotone className="text-[25px] mr-2" />
+          Comics
+        </a>
+        <span>|</span>
+
+        <a href="/movies" className="flex items-center">
+          <PiVideoDuotone className="text-[25px] mr-2" />
+          Movies
+        </a>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
   return (
-    <div className="w-[70%] flex flex-col justify-center mx-auto text-[18px] font-medium">
-      <div>
-        <a href="/comics">Comics</a>
+    <div className="w-[70%] mx-auto text-[30px] font-medium">
+      <div className="flex justify-center m-20 p-8 rounded-[20px] bg-[#f4f4f4]">
+        <a href="/comics">Comics Introduction</a>
       </div>
-      <div>
-        <a href="/movies">Movies</a>
+      <div className="flex justify-center m-20 p-8 rounded-[20px] bg-[#f4f4f4]">
+        <a href="/movies">Movies Introduction</a>
       </div>
     </div>
   )
