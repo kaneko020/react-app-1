e97bd90abaf6ed95919c396217fd0db71193e8e6
kaneko_n <kaneko_n@thinkone.jp>
2023-08-31 11:42:45 +0900

    header レスポンシブ

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,33 +1,31 @@
 import { PiHouseDuotone } from 'react-icons/pi';
 import { PiBooksDuotone } from 'react-icons/pi';
 import { PiVideoDuotone } from 'react-icons/pi';
-import { PiPlugsDuotone } from 'react-icons/pi';
-import { PiPlugsConnectedDuotone } from 'react-icons/pi';
 
 const Header = () => {
   return (
     <div
-      className="flex sticky top-0 left-0 w-full h-[100px] justify-between items-center text-[#333] bg-gray-200 bg-opacity-60 z-10"
+      className="flex sticky top-0 left-0 w-full h-[100px] px-6 justify-between items-center text-[#333] bg-gray-200 bg-opacity-60 z-10"
     >
-      <a href="/" className="flex items-center mx-6 z-20 hover:text-[#555]">
-        <h1 className="text-[35px] font-semibold">Introduction</h1>
+      <a href="/" className="flex items-center z-20 hover:text-[#555]">
+        <h1 className="pc:text-[35px] text-[30px] font-semibold">Introduction</h1>
       </a>
-      <div className="flex items-center mx-8 gap-7 text-[18px] font-medium">
+      <div className="flex items-center mx-2 pc:gap-7 gap-5 text-[18px] font-medium">
         <a href="/" className="flex items-center">
-          <PiHouseDuotone className="text-[25px] mr-2" />
-          Home
+          <PiHouseDuotone className="text-[25px] pc:mr-2" />
+          <p className="pc:flex hidden">Home</p>
         </a>
-        <span>|</span>
+        <span className="pc:flex hidden">|</span>
 
         <a href="/comics" className="flex items-center">
-          <PiBooksDuotone className="text-[25px] mr-2" />
-          Comics
+          <PiBooksDuotone className="text-[25px] pc:mr-2" />
+          <p className="pc:flex hidden">Comics</p>
         </a>
-        <span>|</span>
+        <span className="pc:flex hidden">|</span>
 
         <a href="/movies" className="flex items-center">
-          <PiVideoDuotone className="text-[25px] mr-2" />
-          Movies
+          <PiVideoDuotone className="text-[25px] pc:mr-2" />
+          <p className="pc:flex hidden">Movies</p>
         </a>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
+import { PiMonitorDuotone }  from 'react-icons/pi';
+import { PiMonitorPlayDuotone } from 'react-icons/pi';
+import { PiBookDuotone } from 'react-icons/pi';
+import { PiBookOpenDuotone } from 'react-icons/pi';
+import { PiPlugsDuotone } from 'react-icons/pi';
+import { PiPlugsConnectedDuotone } from 'react-icons/pi';
+
 const Home = () => {
   return (
     <div className="w-[70%] mx-auto text-[30px] font-medium">
-      <div className="flex justify-center m-20 p-8 rounded-[20px] bg-[#f4f4f4]">
-        <a href="/comics">Comics Introduction</a>
-      </div>
-      <div className="flex justify-center m-20 p-8 rounded-[20px] bg-[#f4f4f4]">
-        <a href="/movies">Movies Introduction</a>
-      </div>
+      <a
+        href="/comics"
+        className="flex justify-center items-center m-20 p-8 rounded-[20px] text-white bg-[#4e4e4e] hover:text-[#3d3d3d] hover:bg-[#ececec] hover:transition-all duration-300 ease-in-out"
+      >
+        <PiMonitorDuotone className="text-[50px]" />
+        <p className="mx-3">Comics Introduction</p>
+        <PiMonitorPlayDuotone className="text-[50px]" />
+      </a>
+      <a
+        href="/movies"
+        className="flex justify-center items-center m-20 p-8 rounded-[20px] text-white bg-[#4e4e4e] hover:text-[#3d3d3d] hover:bg-[#ececec] hover:drop-shadow-md"
+      >
+        <PiBookDuotone className="text-[50px]" />
+        <p className="mx-3">Movies Introduction</p>
+        <PiBookOpenDuotone className="text-[50px]" />
+      </a>
     </div>
   )
 }
