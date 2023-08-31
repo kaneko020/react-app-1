ba331843d88f7264ab283116db8ec6933801fc97
kaneko_n <kaneko_n@thinkone.jp>
2023-08-31 12:48:39 +0900

    header レイアウト

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,3 +1,4 @@
+import { PiAppleLogoDuotone } from 'react-icons/pi';
 import { PiHouseDuotone } from 'react-icons/pi';
 import { PiBooksDuotone } from 'react-icons/pi';
 import { PiVideoDuotone } from 'react-icons/pi';
@@ -5,26 +6,28 @@ import { PiVideoDuotone } from 'react-icons/pi';
 const Header = () => {
   return (
     <div
-      className="flex sticky top-0 left-0 w-full h-[100px] px-6 justify-between items-center text-[#333] bg-gray-200 bg-opacity-60 z-10"
+      className="flex sticky top-0 left-0 w-full pc:h-[100px] h-[70px] px-6 justify-between items-center text-[#333] bg-gray-200 bg-opacity-60 z-10"
     >
-      <a href="/" className="flex items-center z-20 hover:text-[#555]">
-        <h1 className="pc:text-[35px] text-[30px] font-semibold">Introduction</h1>
+      <a href="/" className="flex items-center z-20 hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+        <PiAppleLogoDuotone className="text-[40px] mr-2" />
+        <h1 className="pc:flex hidden text-[35px] font-semibold">Introduction</h1>
       </a>
+
       <div className="flex items-center mx-2 pc:gap-7 gap-5 text-[18px] font-medium">
-        <a href="/" className="flex items-center">
-          <PiHouseDuotone className="text-[25px] pc:mr-2" />
+        <a href="/" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+          <PiHouseDuotone className="text-[30px] pc:mr-2" />
           <p className="pc:flex hidden">Home</p>
         </a>
         <span className="pc:flex hidden">|</span>
 
-        <a href="/comics" className="flex items-center">
-          <PiBooksDuotone className="text-[25px] pc:mr-2" />
+        <a href="/comics" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+          <PiBooksDuotone className="text-[30px] pc:mr-2" />
           <p className="pc:flex hidden">Comics</p>
         </a>
         <span className="pc:flex hidden">|</span>
 
-        <a href="/movies" className="flex items-center">
-          <PiVideoDuotone className="text-[25px] pc:mr-2" />
+        <a href="/movies" className="flex items-center hover:scale-[1.1] hover:transition-all duration-300 ease-in-out">
+          <PiVideoDuotone className="text-[30px] pc:mr-2" />
           <p className="pc:flex hidden">Movies</p>
         </a>
       </div>
